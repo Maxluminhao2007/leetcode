172821826c282f0fafd9c4e89ed91b97a8c6bcdc
Minhao <924798079@qq.com>
2019-03-27 16:55:46 +0800

    小修改

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
-var a = [];
-a[5] = 1;
-a[1] = 1;
-
-console.log(a)
+var generate = function(numRows) {
+    let init = [1];
+    let output = [[1]]
+    if(numRows === 1) return output;
+    for(let i = 2; i <= numRows; i++) {
+        //复制一份init
+        let temp = init.concat([]);
+        //init右移一位
+        init.unshift(0);
+        for(let j = 0; j < init.length; j++) {
+            init[j] = temp[j]? (init[j] + temp[j]) :  init[j]
+        }
+        output.push(init.concat([]));
+    }
+    return output;
+};
+generate(5)
