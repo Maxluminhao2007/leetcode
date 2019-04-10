dbd9cfea5ffd3704b62217af0c1649425c437e27
Minhao <924798079@qq.com>
2019-04-10 18:40:57 +0800

    4月10日五道题

--- test.js
+++ test.js
@@ -1,14 +1,2 @@
-var findTheDifference = function(s, t) {
-    let arr = Array(26).fill(0);
 
-    for(let c of s) {
-        arr[c.charCodeAt(0) - 97]++
-    }
-    for(let c of t) {
-        arr[c.charCodeAt(0) - 97]--
-    }
-    for(let c in arr) {
-        if(arr[c]) return c + 97;
-    }
-};
-console.log(findTheDifference('leetcode','leeatcode'))
+console.log(-1 >> 10)
